35c70af319dbb688d6f1ade5830e109801c597b2
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-10 19:36:05 +0100

    made test file

---
+++ server/tests/app.test.ts
@@ -0,0 +1,23 @@
+import request from "supertest";
+import app from "../app";
+
+describe('GET /', () => {
+    it('It loads home page !', async () => {
+        const response = await request(app).get('/')
+        expect(response.status).toBe(200);
+    });
+});
+describe('GET /profiles', () => {
+    it('Loads profiles!', async () => {
+        const response = await request(app).get('/profiles')
+        expect(response.status).toBe(200);
+    });
+});
+describe('GET /profiles/:id', () => {
+    it('It renders individual page', async () => {
+        const userId = 'sampleUserId';
+        const response = await request(app).get(`/profiles/${userId}`);
+        expect(response.status).toBe(200);
+
+    });
+});
