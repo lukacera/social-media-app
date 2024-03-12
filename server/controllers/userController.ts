1e5be870cf1837d6295936d17fe1ac335c92c79b
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-12 16:22:02 +0100

    NewUser controller function created

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import { Types } from "mongoose";
+// User model
 import User from "../models/User";
+
+// User type
+import { userType } from "../types/userType";
 
 // GETTING ALL USERS FROM DB
 export const getAllusers = async (req: Request, res: Response) => {
@@ -21,16 +25,20 @@ export const getUser = async (req: Request, res: Response) => {
 
     try {
         const targetUser = await User.findById(id)
-
+        if (!targetUser) {
+            res.status(404).json({ error: "User not found" })
+            return
+        }
         res.status(200).json({
             user: targetUser
         })
     } catch (error) {
         console.log("Error while getting User: " + error)
-        res.status(404).json({ error: "Error while getting the User from DB" });
+        res.status(404).json({ error: "ID for GETTING USER from DB is invalid" });
     }
 }
 
+// EDIT USER FROM DB
 export const editUser = (req: Request, res: Response) => {
     const id = req.params.id
     if (!Types.ObjectId.isValid(id)) {
@@ -42,6 +50,36 @@ export const editUser = (req: Request, res: Response) => {
     }
 }
 
+// MAKE NEW USER AND INSERT IT INTO DB
+
+export const newUser = async (req: Request, res: Response) => {
+    try {
+        const { age, name, surname, password, username } = req.body
+
+        const usernameTaken = await User.findOne({ username: username })
+        if (!usernameTaken) {
+            const user = new User<userType>({
+                age: age,
+                name: name,
+                surname: surname,
+                password: password,
+                username: username,
+                avatar: req.body.avatar || ""
+            })
+            await user.save()
+
+            res.status(201).json({ message: "User created successfully" })
+            return
+        }
+        // 409 status means that there was a conflict( username is already taken )
+        res.status(409).json({ error: "Username is already taken" });
+
+    } catch (error) {
+        console.log("Error occured on adding newUser to DB")
+        res.status(404).json({ error: "Error while adding new User from DB" })
+    }
+}
+
 // DELETING USER FROM DB
 export const deleteUser = async (req: Request, res: Response) => {
     try {
@@ -49,11 +87,11 @@ export const deleteUser = async (req: Request, res: Response) => {
         await User.deleteOne({ "_id": id });
 
         // Status code for deleting is 204, request has been successful,
-        // but nothing was sent back
+        // but nothing is sent back
         res.status(204).end();
     } catch (error) {
         console.log("Error occurred while trying to delete User from DB! ", error);
-        res.status(404).json({ error: "Error while deleting the User from DB" });
+        res.status(404).json({ error: "Error while deleting User from DB" });
     }
 }
 
