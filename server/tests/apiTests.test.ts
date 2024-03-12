941197f69991f44be5a2eb7a2a4880c82f81aa98
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-12 11:39:21 +0100

    Update tests

--- server/tests/apiTests.test.ts
+++ server/tests/apiTests.test.ts
@@ -1,10 +1,15 @@
 import request from "supertest";
 import app from "../app";
 import { Types } from "mongoose";
+import { Request, Response } from 'express';
 // Types of data
-
+import { userType } from "../types/userType";
 // Models
 import User from "../models/User"
+
+import { newUser } from "../controllers/userController";
+
+
 
 describe('Check "/home" route', () => {
     it('It loads home page, FAIL!', async () => {
@@ -17,8 +22,11 @@ describe('Check "/home" route', () => {
     });
 });
 describe('Check "/profiles" route', () => {
+    beforeEach(async () => {
+        // Clear the User collection
+        await User.deleteMany({});
+    });
     afterEach(() => {
-        // Restore the original implementation after each test
         jest.restoreAllMocks();
     });
     // 1. CLIENT REQUESTS ALL USERS FROM DB
@@ -43,7 +51,9 @@ describe('Check "/profiles" route', () => {
 
 
 describe('Check "/profiles/:id" route', () => {
-
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
     // 1. CLIENT REQUESTS TO GET USER THAT IS NONEXISTING IN DB
     it('GET method single profile, 404', async () => {
         // Wrong id
@@ -101,34 +111,120 @@ describe('Check "/profiles/:id" route', () => {
         const response = await request(app).delete(`/profiles/${userId}`);
         expect(response.status).toBe(204)
     });
-});
+
+    // 1. ADDING NEW USER, WITHOUT ALL REQUIRED PARAMATERS, FAIL SAVING TO DB
+    describe('newUser function', () => {
+        it('should respond with error status if required parameters are missing, FAIL', async () => {
+            const incompleteUserData: Partial<userType> = {
+                // Missing required parameters, function fails because of it
+                age: 30,
+                password: 'password123',
+                username: 'johndoe'
+            };
+
+            // Mock request and response objects
+            const req = { body: incompleteUserData } as unknown as Request;
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            } as unknown as Response;
+
+            await newUser(req, res);
+
+            // Expect status 404 (or appropriate error status) and error message in response
+            expect(res.status).toHaveBeenCalledWith(404);
+            expect(res.json).toHaveBeenCalledWith({ error: 'Error while adding new User from DB' });
+        });
+    });
+
+    // 2. ADDING NEW USER, WITH ALL REQUIRED PARAMATERS, BUT USERNAME IS ALREADY TAKEN
+    describe('newUser function', () => {
+        // Add user with that username in db so it fails
+        beforeAll(async () => {
+            await User.create({
+                name: 'Test',
+                surname: 'User',
+                age: 30,
+                birthday: new Date(),
+                password: 'testPassword',
+                username: 'Ceraa04',
+                avatar: '',
+            });
+        });
+        it('provide all fields for user, but FAIL because of username ', async () => {
+            const completeUser: userType = {
+                name: "Luka",
+                surname: "Ceranic",
+                username: "Ceraa04", // username is already taken
+                age: 28,
+                password: "somePassword"
+            }
+
+            const req = { body: completeUser } as unknown as Request;
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            } as unknown as Response;
+
+            await newUser(req, res);
+
+            // Status 201, user has been saved to DB
+            expect(res.status).toHaveBeenCalledWith(409);
+            expect(res.json).toHaveBeenCalledWith({ error: "Username is already taken" });
+        });
+    });
+    // 3. ADDING NEW USER, WITH ALL REQUIRED PARAMATERS, SAVING TO DB IS SUCCESS
+    describe('newUser function', () => {
+        it('provide all fields for user, SUCCESS ', async () => {
+            const completeUser: userType = {
+                name: "Luka",
+                surname: "Ceranic",
+                username: "RandomUsername", // Username that is not taken
+                age: 28,
+                password: "somePassword"
+            }
+
+            // Mock request and response, so i can test them later on
+            const req = { body: completeUser } as unknown as Request;
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            } as unknown as Response;
+
+            await newUser(req, res);
+
+            // Status 201, user has been saved to DB
+            expect(res.status).toHaveBeenCalledWith(201);
+            expect(res.json).toHaveBeenCalledWith({ message: 'User created successfully' });
+        });
+    });
+    /*
+    describe('Handle friend requests', () => {
+        it("Sends friend request with false ID", async () => {
+            // Wrong id
+            const userId = 'kdfks324324';
+            const response = await request(app).post(`/profiles/${userId}/friend-request`)
+            expect(response.status).toBe(404)
+        })
+        it("Sends friend request, good ID", async () => {
+            // Good id
+            const userId = new Types.ObjectId();
+            const response = await request(app).post(`/profiles/${userId}/friend-request`)
+            expect(response.status).toBe(201);
+        })
 
 
-describe('Handle friend requests', () => {
-    it("Sends friend request with false ID", async () => {
-        // Wrong id
-        const userId = 'kdfks324324';
-        const response = await request(app).post(`/profiles/${userId}/friend-request`)
-        expect(response.status).toBe(404)
-    })
-    it("Sends friend request, good ID", async () => {
-        // Good id
-        const userId = new Types.ObjectId();
-        const response = await request(app).post(`/profiles/${userId}/friend-request`)
-        expect(response.status).toBe(201);
-    })
-
-
-    it("Deletes friend request, bad ID", async () => {
-        // Wrong id
-        const userId = 'kdfks324324';
-        const response = await request(app).delete(`/profiles/${userId}/friend-request`)
-        expect(response.status).toBe(404);
-    })
-    it("Deletes friend request, good ID", async () => {
-        // Good id
-        const userId = new Types.ObjectId();
-        const response = await request(app).delete(`/profiles/${userId}/friend-request`)
-        expect(response.status).toBe(204)
-    })
+        it("Deletes friend request, bad ID", async () => {
+            // Wrong id
+            const userId = 'kdfks324324';
+            const response = await request(app).delete(`/profiles/${userId}/friend-request`)
+            expect(response.status).toBe(404);
+        })
+        it("Deletes friend request, good ID", async () => {
+            // Good id
+            const userId = new Types.ObjectId();
+            const response = await request(app).delete(`/profiles/${userId}/friend-request`)
+            expect(response.status).toBe(204)
+        })
+    }*/
 })
