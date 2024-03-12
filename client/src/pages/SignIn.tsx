0e646fae5205052bd32b481ba8ee0382816f806a
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-12 23:56:37 +0100

    Sign in page styling done

---
+++ client/src/pages/SignIn.tsx
@@ -0,0 +1,47 @@
+import React from "react"
+import { FaUser } from "react-icons/fa";
+
+const SignIn = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="w-[30rem] h-[45rem] bg-gradient-to-tr from-linearGradientStart
+            to-linearGradientEnd text-white flex flex-col gap-20 rounded-lg">
+                <div className="flex flex-col items-center gap-16">
+                    <h2 className="flex pt-10 text-3xl
+                        font-madimi-one tracking-widest">
+                        BONDIFY
+                    </h2>
+                    <p className="text-3xl font-merryweather font-bold">Log in</p>
+                </div>
+                <form method="POST" onClick={(e) => e.preventDefault()}
+                    className="flex flex-col items-center gap-10 w-[80%] mx-auto">
+                    <div className="signInInputWrapper">
+                        <FaUser />
+                        <input className="bg-transparent placeholder:text-slate-200
+                        focus:outline-none"
+                            type="text" placeholder="Username" />
+                    </div>
+                    <div className="signInInputWrapper">
+                        <FaUser />
+                        <input className="bg-transparent placeholder:text-slate-200
+                        focus:outline-none"
+                            type="password" placeholder="Password" />
+                    </div>
+                    <div className="flex flex-col gap-16">
+                        <button className="signInButton">
+                            Login
+                        </button>
+                        <div className="flex flex-col gap-5">
+                            <p>If you don't have account, please sign up below</p>
+                            <button className="signInButton">
+                                Sign up
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+};
+
+export default SignIn;
