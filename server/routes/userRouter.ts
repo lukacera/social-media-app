88c01da3d425fe7a28bef4f3e3d58601c7a961b1
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-12 21:08:09 +0100

    Change controller function names from 'profiles' to 'users'

--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -1,26 +1,32 @@
 import { Router } from "express";
 import { Request, Response } from "express";
-import { Types } from "mongoose";
+
 // Controllers functions
 import { deleteFriendRequest, sendFriendRequest } from "../controllers/friendRequestController";
-import { getProfile, deleteProfile, editProfile } from "../controllers/userController";
+import { getUser, deleteUser, editUser, getAllusers, newUser } from "../controllers/userController";
 const express = require("express");
 const router: Router = express.Router()
 
-router.get("/", (req: Request, res: Response) => {
-    res.status(200).send("Success!")
-})
+// ROUTE FOR HANDLING /PROFILES
+router
+    .route("/")
+    .get(getAllusers)
+    .post(newUser)
+
+// ROUTES FOR HANDLING USER'S User
 
 router
     .route("/:id")
-    .get(getProfile)
-    .patch(editProfile)
-    .delete(deleteProfile)
+    .get(getUser)
+    .patch(editUser)
+    .delete(deleteUser)
 
 // ROUTES FOR HANDLING FRIEND REQUEST
+
 router
     .route("/:id/friend-request")
     .post(sendFriendRequest)
     .delete(deleteFriendRequest)
+
 
 export default router;
