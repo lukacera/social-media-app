5dd03f16d8654e59769066b291eefb1d9e5b6dcf
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-15 20:54:03 +0100

    If no user is logged in, replace profile wrapper for current user with LOGIN btn

--- client/src/components/CurrentUser.tsx
+++ client/src/components/CurrentUser.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import { userType } from "../../../server/types/userType";
 import { getCurrentUser } from "../api/getCurrentUserApi";
 import { Link } from "react-router-dom";
 const CurrentUser: React.FC = () => {
+    const token = localStorage.getItem("token");
     interface CurrentUser {
         username: string,
         avatar: string
@@ -23,36 +23,50 @@ const CurrentUser: React.FC = () => {
         fetchData()
     }, [])
     return (
-        <div className="dropdown grid place-items-center">
-            <div className="profileWrapper">
-                <img className="w-12 h-12 rounded-full"
-                    src={currentUserData.avatar} alt="pic" />
-                <p className="profileName">{currentUserData.username}</p>
-            </div>
-            <div className="menu h-[10rem] w-[20rem] 
-            bg-profileColor border-[1px] border-borderGray
-            absolute top-28 right-5 duration-300 font-exo
-            hidden rounded-lg grid-rows-[25%_75%]">
-                <p className="flex justify-center pt-3 text-[1.3rem] 
-                font-bold tracking-wider">
-                    <span></span>
-                </p>
-                <div className="flex justify-center items-center gap-5">
-                    <Link to={`/users/${currentUserData.username}`}>
-                        <p className="dropdownButton">
-                            Edit profile
+        <>
+            {token && (
+                <div className="dropdown grid place-items-center">
+                    <div className="profileWrapper">
+                        <img className="w-12 h-12 rounded-full"
+                            src={currentUserData.avatar} alt="pic" />
+                        <p className="profileName">{currentUserData.username}</p>
+                    </div>
+                    <div className="menu h-[10rem] w-[20rem] rounded-lg 
+                    bg-profileColor border-[1px] border-borderGray
+                    absolute top-28 hidden ">
+                        <p className="flex justify-center pt-3 text-[1.3rem] 
+                        font-bold tracking-wider">
+                            <span></span>
                         </p>
-                    </Link>
-                    <Link to={"/login"} onClick={() => localStorage.removeItem("token")}>
-                        <p className="dropdownButton">
-                            Logout
-                        </p>
-                    </Link>
+                        <div className="flex justify-center gap-5">
+                            <Link to={`/users/${currentUserData.username}`}>
+                                <p className="dropdownButton">
+                                    Edit profile
+                                </p>
+                            </Link>
+                            <Link to={"/login"} onClick={() => localStorage.removeItem("token")}>
+                                <p className="dropdownButton">
+                                    Logout
+                                </p>
+                            </Link>
 
+                        </div>
+                    </div>
                 </div>
 
-            </div>
-        </div>
+            )}
+            {!token && (
+                <div className="grid place-items-center">
+                    <Link to={"/login"}>
+                        <p className="px-5 py-3 text-xl font-[Nunito] tracking-wide  
+                        bg-profileColor border-borderGray border-[1px]
+                        hover:bg-white hover:text-black hover:border-none">
+                            LOGIN
+                        </p>
+                    </Link>
+                </div>
+            )}
+        </>
     )
 };
 
