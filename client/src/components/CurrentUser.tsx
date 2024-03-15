866c732f3c430af9107ab8c5dd34fc841ffdaa7b
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-15 21:00:32 +0100

    Clean code for currentUser

--- client/src/components/CurrentUser.tsx
+++ client/src/components/CurrentUser.tsx
@@ -2,15 +2,22 @@ import { useEffect, useState } from "react";
 import { getCurrentUser } from "../api/getCurrentUserApi";
 import { Link } from "react-router-dom";
 const CurrentUser: React.FC = () => {
+    // Get token from local storage
     const token = localStorage.getItem("token");
+
+    // Declare type 
     interface CurrentUser {
         username: string,
         avatar: string
     }
+
+    // Initialize states for current user's avatar and username 
     const [currentUserData, setCurrentUserData] = useState<CurrentUser>({
         avatar: "",
         username: ""
     })
+
+    // Fetch data from server
     useEffect(() => {
         const fetchData = async () => {
             const fetched_data = await getCurrentUser();
@@ -24,6 +31,7 @@ const CurrentUser: React.FC = () => {
     }, [])
     return (
         <>
+            {/* If user is logged in, display his profileWrapper */}
             {token && (
                 <div className="dropdown grid place-items-center">
                     <div className="profileWrapper">
@@ -33,28 +41,25 @@ const CurrentUser: React.FC = () => {
                     </div>
                     <div className="menu h-[10rem] w-[20rem] rounded-lg 
                     bg-profileColor border-[1px] border-borderGray
-                    absolute top-28 hidden ">
-                        <p className="flex justify-center pt-3 text-[1.3rem] 
-                        font-bold tracking-wider">
-                            <span></span>
-                        </p>
-                        <div className="flex justify-center gap-5">
+                    absolute top-28 hidden place-items-center">
+                        <div className="flex gap-5">
                             <Link to={`/users/${currentUserData.username}`}>
                                 <p className="dropdownButton">
                                     Edit profile
                                 </p>
                             </Link>
+                            {/* Remove token from localStorage when user clicks on logout button & redirect to login */}
                             <Link to={"/login"} onClick={() => localStorage.removeItem("token")}>
                                 <p className="dropdownButton">
                                     Logout
                                 </p>
                             </Link>
-
                         </div>
                     </div>
                 </div>
 
             )}
+            {/* If no user is logged in, instead of profileWrapper, put login button */}
             {!token && (
                 <div className="grid place-items-center">
                     <Link to={"/login"}>
