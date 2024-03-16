4d0bdfa5c9dbf8913565b8aaeeee7123c0465500
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-16 08:08:37 +0100

    Error page, when URL is invalid or user is not authorized to reach that URL

---
+++ client/src/pages/ErrorPage.tsx
@@ -0,0 +1,17 @@
+import "../assets/index.css";
+
+import HeaderNav from "../components/HeaderNav";
+
+const ErrorPage = () => {
+    return (
+        <div className="grid grid-rows-[15%,85%] h-screen bg-white
+            dark:bg-backgroundDark text-white font-[Nunito]">
+            <HeaderNav />
+            <p className="pageNotFoundParagraph">
+                Page not found!
+            </p>
+        </div>
+    )
+};
+
+export default ErrorPage
