ae8d6d2b4815191d13527b56d30802fe36159ef2
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-17 19:45:03 +0100

    Add function for deleting friend request in controller

--- server/controllers/friendRequestController.ts
+++ server/controllers/friendRequestController.ts
@@ -1,10 +1,7 @@
 import { Request, Response } from "express";
 const asyncHandler = require("express-async-handler")
-import { Types } from "mongoose";
 import User from "../models/User"
 import { userType } from "../types/userType";
-
-
 
 
 interface CustomRequest extends Request {
@@ -15,10 +12,13 @@ interface CustomRequest extends Request {
 // @route POST "/api/users/:username/friendRequest"
 
 export const sendFriendRequest = asyncHandler(async (req: CustomRequest, res: Response) => {
+
     const username = req.params.username;
+
+    // Get currentUser by JWT, from protect middleware
     const currentUser = req.user;
 
-    // Find user in DB by username, and populate friendRequests
+    // Find user in DB by username
     const targetUser = await User.findOne({ username: username })
 
     // Variable that will track if either current user and target user are friends or current user
@@ -29,7 +29,7 @@ export const sendFriendRequest = asyncHandler(async (req: CustomRequest, res: Re
         return res.status(400).json({ error: "User cannot be friends with himself!" })
     }
     if (!targetUser?.username) {
-        return res.status(400).json({ error: "Target user not found!" })
+        return res.status(400).json({ error: "Target user not found" })
     }
 
 
@@ -69,14 +69,38 @@ export const sendFriendRequest = asyncHandler(async (req: CustomRequest, res: Re
     }
 
 })
+
 // @desc  Get user from DB by username
 // @route DELETE "/api/users/:username/friendRequest"
-export const deleteFriendRequest = (req: Request, res: Response) => {
-    const id = req.params.id;
-    if (!Types.ObjectId.isValid(id)) {
-        res.status(404).end()
-    } else {
-        res.status(204).end()
+
+export const deleteFriendRequest = asyncHandler(async (req: CustomRequest, res: Response) => {
+
+    const username = req.params.username;
+
+    // Find user in DB by username
+    const targetUser = await User.findOne({ username: username })
+
+    if (!targetUser) {
+        return res.status(404).json({ message: "Target user not found" })
     }
-};
+
+    // Get currentUser from protect middleware
+    const currentUser = req.user
+
+    // Check if user sent request to targetUser
+    if (targetUser?.friendRequests?.includes(currentUser.username)) {
+        await User.updateOne(
+            { username: targetUser.username },
+            { $pull: { friendRequests: currentUser.username } }
+        );
+        return res.status(200).json({
+            message: `User ${currentUser.username} successfully deleted his friend request for ${targetUser?.username}`
+        });
+    }
+
+    // If user never sent a request to targetUser, return status 400
+    return res.status(400).json({
+        message: `Request was not deleted, because ${currentUser.username} never sent friend request to ${targetUser?.username}`
+    });
+});
 
