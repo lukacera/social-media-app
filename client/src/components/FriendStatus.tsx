be5b860df093aa989bd8a4547f9c046e985f6c64
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-18 19:17:11 +0100

    Initialize useEffect hook in friendStatus to set friend request status for each profile in allProfiles page, to prevent bug that resets isFriendRequestSent state to false on refresh of page

--- client/src/components/FriendStatus.tsx
+++ client/src/components/FriendStatus.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IoMdPersonAdd } from "react-icons/io";
 import { FaUserFriends } from "react-icons/fa";
 import { TiCancel } from "react-icons/ti";
-import { deleteFriendRequest } from "../api/deleteFriendRequestApi";
+import { deleteSentFriendRequest } from "../api/deleteSentFriendRequestApi";
 import { sendFriendRequest } from '../api/sendFriendRequestApi';
 import { userType } from '../../../server/types/userType';
 
@@ -12,12 +12,19 @@ const FriendStatus: React.FC<{ currentUser: userType, targetUser: userType }> = 
     const [isFriendRequestSent, setIsFriendRequestSent] = useState(
         targetUser.friendRequests?.includes(currentUser.username)
     );
+
+    // Initialize useEffect hook to set friend request status for each user on each render
+    useEffect(() => {
+        setIsFriendRequestSent(
+            targetUser.friendRequests?.includes(currentUser.username)
+        )
+    }, [targetUser, currentUser.username])
+
     const areFriends = currentUser.friends?.includes(targetUser.username)
-    console.log(isFriendRequestSent)
     const handleFriendRequest = async () => {
         if (isFriendRequestSent) {
             // Cancel friend request
-            const data = await deleteFriendRequest(targetUser.username);
+            const data = await deleteSentFriendRequest(targetUser.username);
             console.log("Delete request!")
             console.log(data)
             setIsFriendRequestSent(false);
