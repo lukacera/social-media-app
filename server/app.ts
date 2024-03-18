8413d4924d0e5a40c7932cdff24c9ac6591c13e6
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-18 09:57:23 +0100

    Add friendRequest router to main app

--- server/app.ts
+++ server/app.ts
@@ -4,12 +4,16 @@ const cors = require("cors")
 import { Application, Request, Response } from "express";
 const app: Application = express()
 require("dotenv").config()
-// DB connection functiondx
+
+
+// DB connection function
 import { connectToDB } from "./config/connectDB";
 // Routers
 import userRouter from "./routes/userRouter"
 import authRouter from "./routes/authRouter"
-import { protect } from "./middlewares/authMiddleware";
+import friendReqRouter from "./routes/friendRequestRouter"
+
+
 connectToDB()
 
 app.use(cors())
@@ -23,15 +27,18 @@ app.get("/home", (req: Request, res: Response) => {
     res.status(200).end()
 });
 
-// Handling users Router
-app.use("/api/users", userRouter)
+app.use("/api/users", friendReqRouter, userRouter)
 
 app.use("/api/auth", authRouter)
+
+
 
 // If app did not recognize route till this point, it means it's 404
 app.get("*", (req: Request, res: Response) => {
     res.status(404).send("Page does not exists")
 })
+
+
 const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log("Server is live!")
