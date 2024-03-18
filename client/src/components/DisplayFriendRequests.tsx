3f90dd111f436d5f2944639927d859c9412209f0
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-18 19:14:59 +0100

    Set dropdown menu for displayFriendRequest component and handle accept and delete friend request cases

--- client/src/components/DisplayFriendRequests.tsx
+++ client/src/components/DisplayFriendRequests.tsx
@@ -3,10 +3,14 @@ import { getCurrentUser } from "../api/getCurrentUserApi";
 import { IoMdPersonAdd } from "react-icons/io";
 import { TiCancel } from "react-icons/ti";
 import { FaCheck } from "react-icons/fa";
+import { acceptFriendRequest } from "../api/acceptFriendRequestApi";
+import { deleteReceivedFriendRequest } from "../api/deleteRecievedFriendRequestApi";
 
 const DisplayFriendRequests: React.FC = () => {
+
     const [friendRequests, setFriendRequests] = useState<string[]>([])
     const [dropdown, setDropdown] = useState<boolean>(false)
+
     // Fetch friendRequests for user that is currently logged in
     useEffect(() => {
         const fetchData = async () => {
@@ -24,6 +28,30 @@ const DisplayFriendRequests: React.FC = () => {
         fetchData()
     }, [])
 
+    const handleAcceptFriendRequest = async (request: string, index: number) => {
+        try {
+            await acceptFriendRequest(request)
+            setFriendRequests((prevRequests) => {
+                const updatedRequests = [...prevRequests];
+                updatedRequests.splice(index, 1)
+                return updatedRequests
+            })
+        } catch (error) {
+            console.log("Error occured while accepting friend request: " + error)
+        }
+    }
+    const handleDeleteFriendRequest = async (request: string, index: number) => {
+        try {
+            await deleteReceivedFriendRequest(request)
+            setFriendRequests((prevRequests) => {
+                const updatedRequests = [...prevRequests];
+                updatedRequests.splice(index, 1)
+                return updatedRequests
+            })
+        } catch (error) {
+            console.log("Error occured while deleting friend request: " + error)
+        }
+    }
     return (
         <section>
             <div className="relative">
@@ -35,27 +63,48 @@ const DisplayFriendRequests: React.FC = () => {
                     <span className="text-xl">{friendRequests.length}</span>
                 </p>
                 {dropdown && (
-                    <ul className="absolute -left-24 top-10 px-7 py-5 bg-profileColor
-                                    border-2 border-borderGray z-10">
-                        {friendRequests.map((request, index) => (
-                            <li key={index} className="flex gap-5 items-center">
-                                <span className="text-[1.2rem]">
-                                    {request}
+                    <div className="absolute -left-24 top-14 px-7 py-5 bg-profileColor
+                    border-2 border-borderGray z-10">
+                        {friendRequests.length > 0 && (
+                            <h3 className="text-lg text-nowrap">
+                                You have
+                                <span className="font-bold px-1">
+                                    {friendRequests.length}
                                 </span>
-                                <div className="flex text-2xl gap-3">
-                                    <span className="border-2 p-2 rounded-full cursor-pointer
-                                    hover:bg-white hover:text-black">
-                                        < TiCancel />
-                                    </span>
-                                    <span className="border-2 p-2 rounded-full cursor-pointer
-                                    hover:bg-white hover:text-black">
-                                        < FaCheck />
-                                    </span>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+                                pending
+                                <span className="px-1">
+                                    {friendRequests.length === 1 ? "request" : "requests"}
+                                </span>
 
+                            </h3>
+                        )}
+                        {friendRequests.length === 0 && (
+                            <span className="text-lg text-nowrap">
+                                You have no pending friend requests!
+                            </span>
+                        )}
+                        <ul className="pt-8">
+                            {friendRequests.map((request, index) => (
+                                <li key={index} className="flex gap-5 items-center">
+                                    <span className="text-[1.2rem]">
+                                        {request}
+                                    </span>
+                                    <p className="flex text-2xl gap-3">
+                                        <span className="border-2 border-red-600 p-2 rounded-full cursor-pointer
+                                        text-red-600 hover:bg-white hover:border-white"
+                                            onClick={() => handleDeleteFriendRequest(request, index)}>
+                                            < TiCancel title="Decline request" />
+                                        </span>
+                                        <span className="border-2 border-green-600 p-2 rounded-full cursor-pointer
+                                        text-green-600 hover:bg-white hover:border-white"
+                                            onClick={() => handleAcceptFriendRequest(request, index)}>
+                                            < FaCheck title="Accept request" />
+                                        </span>
+                                    </p>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 )}
             </div>
         </section>
