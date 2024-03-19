0e05dcd08aef55e311c8ce7f40080d0769baab11
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-19 19:38:27 +0100

    Create API for editing currentUser's profile

---
+++ client/src/api/editProfileApi.ts
@@ -0,0 +1,28 @@
+import { userType } from "../../../server/types/userType";
+import { baseUrl } from "../constants/baseURL";
+
+// Username and password are not updatable
+type UpdateType = Omit<Omit<userType, "password">, "username">;
+
+export const editProfile = async (targetUsername: string, update: UpdateType) => {
+    const token = localStorage.getItem("token")
+    try {
+        const response = await fetch(`${baseUrl}/users/${targetUsername}`, {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify(update)
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to delete request');
+        }
+
+        const data = response.json()
+        return data;
+    } catch (error) {
+        throw new Error('Operation failed!');
+    }
+};
