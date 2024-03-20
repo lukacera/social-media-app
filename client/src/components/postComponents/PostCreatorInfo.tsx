76fd8d7f483d4724f744f72995cce0610f5600b9
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-20 18:31:48 +0100

    Link to profile page

--- client/src/components/postComponents/PostCreatorInfo.tsx
+++ client/src/components/postComponents/PostCreatorInfo.tsx
@@ -2,15 +2,17 @@ import React from "react"
 import { getAvatarURL } from "../../constants/avatarURL";
 import moment from "moment";
 import { postType } from "../../../../server/types/postType";
-
+import { Link } from "react-router-dom";
 const PostCreatorInfo: React.FC<{ post: postType }> = ({ post }) => {
     return (
-        <div className="flex flex-col items-center
-                                        gap-10 cursor-pointer">
-            <div className="flex flex-col items-center gap-5">
-                <img className="w-[4rem] h-[4rem] rounded-full"
-                    src={getAvatarURL(post.creator?.avatar || "")} alt="" />
-                <p className="text-lg">{post.creator.username}</p>
+        <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center gap-3">
+                <Link className="grid gap-3 place-items-center"
+                    to={`/users/${post.creator.username}`}>
+                    <img className="w-[4rem] h-[4rem] rounded-full"
+                        src={getAvatarURL(post.creator?.avatar || "")} alt="" />
+                    <p className="text-lg">{post.creator.username}</p>
+                </Link>
                 <p>{moment(post.postCreatedAt).format('MMMM Do YYYY, h:mm:ss a')}</p>
             </div>
         </div>
