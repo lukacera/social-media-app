dc10841ebc5d7fbb7d3681b7560ac8f36cc898dd
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-20 17:56:37 +0100

    Api for fetching all posts created

---
+++ client/src/api/getAllPostsApi.ts
@@ -0,0 +1,21 @@
+import { baseUrl } from "../constants/baseURL";
+export const getAllPosts = async () => {
+
+    try {
+        const response = await fetch(`${baseUrl}/posts/getAllPosts`, {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch posts');
+        }
+        const data = await response.json();
+        console.log(data)
+        return data.posts;
+    } catch (error) {
+        throw new Error('Operation failed!');
+    }
+};
