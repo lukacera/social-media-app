e3eec3012ad3693e15cf661fa2c96dd38b2d86ed
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-20 17:47:53 +0100

    Add Overlay component

---
+++ client/src/components/Overlay.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Overlay: React.FC = () => {
+    return (
+        <div className="fixed w-full h-full top-0 right-0 bg-black/50">
+        </div>
+    )
+};
+
+export default Overlay;
