af0cdbe9c2824ad294e4abb87f6d5992367c992f
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-21 12:05:30 +0100

    Fix type for userData posts

--- client/src/components/viewProfileComponents/UserAllPosts.tsx
+++ client/src/components/viewProfileComponents/UserAllPosts.tsx
@@ -1,29 +1,22 @@
 import React from 'react';
 import { userType } from "../../../../server/types/userType";
-
 import { postType } from '../../../../server/types/postType';
 
 import SinglePostComponent from '../postComponents/SinglePostComponent';
-// Posts are originally ObjectID[] type, so they do not have postType types 
-type UserDataWithPosts = userType & {
-    posts?: postType[];
-};
 
 
-const UserAllPosts: React.FC<{ userData: UserDataWithPosts }> = ({ userData }) => {
+const UserAllPosts: React.FC<{ userData: userType }> = ({ userData }) => {
     return (
         <div className="my-12">
-            {userData.posts && (
-                <div>
-                    {userData.posts.map((post: postType, index: number) => (
-                        <div key={index}>
-                            < SinglePostComponent post={post} />
-                        </div>
-                    ))}
+            {/* Convert type from array of ObjectID to array of postTypes */}
+            {(userData.posts as unknown as postType[])?.map((post: postType, index: number) => (
+                <div key={index}>
+                    <SinglePostComponent post={post} />
                 </div>
-            )}
+            ))}
         </div>
-    )
+    );
 };
+
 
 export default UserAllPosts;
