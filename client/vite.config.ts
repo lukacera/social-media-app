365e0d291237ecd86faa83447ecf783a6b26ef56
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-22 11:18:41 +0100

    dgdfg

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "https://social-media-app-9qzn-47u5s2io6-ceraa04s-projects.vercel.app"
+    }
+  },
   plugins: [react()],
 })
