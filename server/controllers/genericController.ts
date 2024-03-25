d38cafcca261c8fe70b2fa944d457e71fe672bbe
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-25 21:53:22 +0100

    Populate nested paths with .

--- server/controllers/genericController.ts
+++ server/controllers/genericController.ts
@@ -1,15 +1,29 @@
-import { Model } from "mongoose";
+import { Model, Document } from "mongoose";
 import { Request, Response } from "express";
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
 
-export const getAllDocuments = <T>(Model: Model<T>, populateField?: string) =>
+export const getAllDocuments = <T>(
+    Model: Model<T>,
+    populateFields: string[]
+) =>
     asyncHandler(async (req: Request, res: Response) => {
         let documents;
-        if (populateField) {
-            documents = await Model.find().populate(populateField);
+
+        if (populateFields && populateFields.length > 0) {
+            // If populateFields is provided and not empty
+            const populateOptions = populateFields.map(field => {
+                // Check if field is nested
+                if (field.includes(".")) {
+                    // Split nested field by dot and populate each part
+                    const [firstField, ...restFields] = field.split(".");
+                    return { path: firstField, populate: { path: restFields.join(".") } };
+                } else {
+                    return { path: field };
+                }
+            });
+            documents = await Model.find().populate(populateOptions);
         } else {
             documents = await Model.find();
-
         }
 
         // Name field is plural, lowercase version of Model.modelname
