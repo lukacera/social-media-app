01088e1a065b778dc455400c8b1eb6d602d02d6e
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-25 21:55:19 +0100

    Fetch all posts from user

--- client/src/components/viewProfileComponents/UserAllPosts.tsx
+++ client/src/components/viewProfileComponents/UserAllPosts.tsx
@@ -1,18 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { postType } from '../../../../server/types/postType';
 
 import SinglePostComponent from '../postComponents/SinglePostComponent';
 import { UserContext } from '../../hooks/UserContextHook';
+import { getAllPosts } from '../../api/postAPIs/getAllPostsApi';
 
 const UserAllPosts: React.FC = () => {
     // Get currentUserData from Context Provider
-    const { targetUser } = useContext(UserContext)
+    const { targetUser } = useContext(UserContext);
+    const [userPosts, setUserPosts] = useState<postType[]>([]);
+
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const posts: postType[] = await getAllPosts();
+                // Filter and reverse, reverse is for showing latest posts first
+                const filteredPosts = posts.filter(post => post.creator._id === targetUser._id).reverse()
+                setUserPosts(filteredPosts)
+            }
+            catch (error) {
+                console.error("Error occured while fetching all posts from DB: " + error)
+            }
+        }
+        fetchPosts()
+    }, [userPosts, targetUser._id]);
 
     return (
         <div className="my-12 grid place-items-center gap-20">
-
-            {/* Convert type from array of ObjectID to array of postTypes */}
-            {(targetUser.posts as unknown as postType[])?.map((post: postType, index: number) => (
+            {userPosts.map((post: postType, index: number) => (
                 <div key={index}>
                     <SinglePostComponent post={post} />
                 </div>
@@ -21,5 +37,6 @@ const UserAllPosts: React.FC = () => {
     );
 };
 
-
 export default UserAllPosts;
+
+
