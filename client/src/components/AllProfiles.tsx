12849c90b09c3e889acd1b86eb6df83f089d7629
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-28 19:45:11 +0100

    Add loading state to allProfilkes component

--- client/src/components/AllProfiles.tsx
+++ client/src/components/AllProfiles.tsx
@@ -1,35 +1,29 @@
 import "../assets/index.css";
-
-import { useContext, useEffect, useState } from "react";
+import { Key, useContext, useEffect, useState } from "react";
 import { getAllProfiles } from "../api/fetchUsersAPIs/getAllProfilesApi";
 import { userType } from "../../../server/types/userType";
-
 import { UserContext } from "../hooks/UserContextHook";
 import AllProfilesSearchBarInput from "./allProfilesComponents/AllProfilesSearchBarInput";
 import Profile from "./allProfilesComponents/Profile";
 
 const AllProfiles: React.FC = () => {
 
-    const { currentUserData } = useContext(UserContext)
+    const { currentUserData } = useContext(UserContext);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [users, setUsers] = useState<userType[]>([]);
+    const [searchProfile, setSearchProfile] = useState<string>('');
 
-    const [loading, setLoading] = useState<boolean>(true)
-    const [users, setUsers] = useState<userType[]>([])
-    const [searchProfile, setSearchProfile] = useState<string>('')
-
-    // Fetch all profiles on first render, and set loading to false
     useEffect(() => {
         const fetchUsers = async () => {
             try {
                 const data = (await getAllProfiles()).filter(user => (
                     user.username !== currentUserData.username
-                ))
-
-                setUsers(data)
-                setLoading(false)
-
-            }
-            catch (error) {
-                console.error("Error occured while fetching all profiles from DB: " + error)
+                ));
+                setUsers(data);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error occurred while fetching all profiles from DB: ", error);
+                setLoading(false);
             }
         }
         fetchUsers()
@@ -44,20 +38,20 @@ const AllProfiles: React.FC = () => {
 
     return (
         <div>
-            {!loading && (
+            {loading ? (
+                <div className="text-center mt-20">Loading...</div>
+            ) : (
                 <div className="my-20 flex flex-col gap-32 items-center overflow-auto">
                     <AllProfilesSearchBarInput setSearchProfile={setSearchProfile} />
                     <div className="flex flex-wrap gap-20 justify-center">
 
                         {filteredUsers.length === 0 &&
-                            <p className="text-2xl">No profiles found!</p>
-                        }
-
-                        {/* Display filteredUser (all but currentUser) */}
-                        {filteredUsers && filteredUsers.map((user: userType, index: number) => (
-
-                            <div key={index}>
-                                < Profile user={user} />
+                            <p className="text-2xl">
+                                No profiles found!
+                            </p>}
+                        {filteredUsers.map((user: userType) => (
+                            <div key={user._id as unknown as Key}>
+                                <Profile user={user} />
                             </div>
 
                         ))}
