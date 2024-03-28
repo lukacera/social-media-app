a02ccf2767c0c893c5d5684adfdce69bff4964af
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-28 19:33:48 +0100

    Update both friendStatus on viewProfile and allProfiles pages

--- client/src/components/allProfilesComponents/FriendStatus.tsx
+++ client/src/components/allProfilesComponents/FriendStatus.tsx
@@ -1,53 +1,115 @@
 import { useContext, useEffect, useState } from 'react';
 import { IoMdPersonAdd } from "react-icons/io";
-import { FaUserFriends } from "react-icons/fa";
+import { FaUserFriends, FaCheck } from "react-icons/fa";
 import { TiCancel } from "react-icons/ti";
 import { deleteSentFriendRequest } from "../../api/friendRequestAPIs/deleteSentFriendRequestApi";
 import { sendFriendRequest } from '../../api/friendRequestAPIs/sendFriendRequestApi';
+import { acceptFriendRequest } from '../../api/friendRequestAPIs/acceptFriendRequestApi';
 import { userType } from '../../../../server/types/userType';
 import { UserContext } from '../../hooks/UserContextHook';
 
 const FriendStatus: React.FC<{ targetUser: userType }> = ({ targetUser }) => {
-    const { currentUserData } = useContext(UserContext)
+    const { currentUserData } = useContext(UserContext);
+
     // Initialize state to check if currentUser and targetUser are friends
-    const [isFriendRequestSent, setIsFriendRequestSent] = useState(
-        targetUser.friendRequests?.includes(currentUserData.username)
-    );
+    const [areFriends, setAreFriends] = useState<boolean>(false);
+    const [friendRequestSent, setFriendRequestSent] = useState<boolean>(false);
+    const [friendRequestReceived, setFriendRequestReceived] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
 
-    // Initialize useEffect hook to set friend request status for each user on each render
+
     useEffect(() => {
-        setIsFriendRequestSent(
-            targetUser.friendRequests?.includes(currentUserData.username)
-        )
-    }, [targetUser, currentUserData.username])
 
-    // Check if users are friends already
-    const areFriends = currentUserData.friends?.includes(targetUser.username)
+        // Check if they are friends
+        if (currentUserData.friends?.includes(targetUser.username) && targetUser.friends?.includes(currentUserData.username)) {
+            setAreFriends(true);
+            setFriendRequestSent(false)
+            setFriendRequestReceived(false)
+        }
 
+        // If they are not friends
+        else {
+            if (targetUser.friendRequests && targetUser.friendRequests.includes(
+                currentUserData.username)) {
+
+                setFriendRequestSent(true)
+                setAreFriends(false);
+                setFriendRequestReceived(false);
+            }
+
+            else if (currentUserData.friendRequests && currentUserData.friendRequests.includes(
+                targetUser.username)) {
+                setFriendRequestReceived(true);
+
+                setAreFriends(false);
+                setFriendRequestSent(false);
+            }
+
+            else { // Neither user sent request to another, nor they are friends
+
+                setFriendRequestReceived(false);
+                setAreFriends(false);
+                setFriendRequestSent(false);
+            }
+        }
+
+    }, [currentUserData, targetUser]);
+
+    // Function that will be called when user clicks on button
     const handleFriendRequest = async () => {
-        if (isFriendRequestSent) {
-            // Cancel friend request
-            await deleteSentFriendRequest(targetUser.username);
+        setLoading(true); // Start loading
 
-            setIsFriendRequestSent(false);
-        } else {
-            // Send friend request
-            setIsFriendRequestSent(true);
-            await sendFriendRequest(targetUser.username)
+        try {
+
+            if (areFriends) {
+                // If they are already friends, delete the sent friend request
+                await deleteSentFriendRequest(targetUser.username);
+                setAreFriends(false);
+                setFriendRequestSent(false);
+                setFriendRequestReceived(false);
+            }
+
+            else if (friendRequestSent) {
+                // If a friend request is sent, cancel it
+                await deleteSentFriendRequest(targetUser.username);
+                setFriendRequestSent(false);
+            }
+
+            else if (friendRequestReceived) {
+                // If a friend request is received, accept it
+                await acceptFriendRequest(targetUser.username);
+                setAreFriends(true);
+                setFriendRequestReceived(false);
+            }
+
+            else {
+                // If no friend request is sent or received, send a friend request
+                await sendFriendRequest(targetUser.username);
+                setFriendRequestSent(true);
+            }
+        } catch (error) {
+            console.error("Error occurred while handling friend request: ", error);
+        } finally {
+            setLoading(false); // Stop loading
         }
     };
 
     return (
         <div className="flex justify-end w-full">
-            <p className="bg-white px-2 text-black rounded-full text-2xl" onClick={handleFriendRequest}>
+            <button
+                className="bg-white px-2 text-black rounded-full text-2xl"
+                onClick={handleFriendRequest}
+                disabled={loading}>
                 {areFriends ? (
                     <FaUserFriends />
-                ) : isFriendRequestSent ? (
+                ) : friendRequestSent ? (
                     <TiCancel />
+                ) : friendRequestReceived ? (
+                    <FaCheck />
                 ) : (
                     <IoMdPersonAdd />
                 )}
-            </p>
+            </button>
         </div>
     );
 };
---
+++ client/src/components/viewProfileComponents/FriendStatusViewProfilePage.tsx
@@ -0,0 +1,184 @@
+import React, { useContext, useState, useEffect } from "react";
+import { UserContext } from "../../hooks/UserContextHook";
+import { sendFriendRequest } from "../../api/friendRequestAPIs/sendFriendRequestApi";
+import { deleteSentFriendRequest } from "../../api/friendRequestAPIs/deleteSentFriendRequestApi";
+import { deleteReceivedFriendRequest } from "../../api/friendRequestAPIs/deleteRecievedFriendRequestApi";
+import { socket } from "../../constants/SocketIoURL";
+import { acceptFriendRequest } from "../../api/friendRequestAPIs/acceptFriendRequestApi";
+import { unfriendUser } from "../../api/friendRequestAPIs/unfriendApi";
+import { userType } from "../../../../server/types/userType";
+
+const FriendStatusViewProfilePage: React.FC<{ targetUser: userType }> = ({ targetUser }) => {
+
+
+  const { currentUserData } = useContext(UserContext);
+
+  const [areFriends, setAreFriends] = useState<boolean>(false);
+  const [friendRequestSent, setFriendRequestSent] = useState<boolean>(false);
+  const [friendRequestReceived, setFriendRequestReceived] = useState<boolean>(false);
+
+
+  useEffect(() => {
+
+    // Set initial state for areFriends
+    currentUserData.friends && setAreFriends(currentUserData.friends.includes(targetUser.username));
+
+    // Set initial state for friendRequestSent
+    targetUser.friendRequests && setFriendRequestSent(targetUser.friendRequests.includes(currentUserData.username));
+
+    // Set initial state for friendRequestReceived
+    currentUserData.friendRequests && setFriendRequestReceived(currentUserData.friendRequests.includes(targetUser.username));
+
+  }, [targetUser, currentUserData]);
+
+
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+
+    socket.on("friendRequestSent", () => {
+      setFriendRequestSent(true);
+      setAreFriends(false);
+      setFriendRequestReceived(false);
+    });
+
+    socket.on("deleteSentFriendRequest", () => {
+      setFriendRequestSent(false);
+    });
+
+    socket.on("unfriendUser", () => {
+      setAreFriends(false);
+      setFriendRequestSent(false);
+      setFriendRequestReceived(false);
+    });
+
+    socket.on("deleteReceivedFriendRequest", () => {
+      setFriendRequestReceived(false);
+    });
+
+    return () => {
+      socket.off("friendRequestSent");
+      socket.off("acceptFriendRequest");
+      socket.off("unfriendUser");
+      socket.off("deleteSentFriendRequest");
+      socket.off("deleteReceivedFriendRequest");
+    };
+
+  }, []);
+
+  const handleUnfriend = async () => {
+    setLoading(true);
+    try {
+      await unfriendUser(targetUser.username);
+      setAreFriends(false);
+      setFriendRequestReceived(false)
+      setFriendRequestSent(false)
+    } catch (error) {
+      console.error("Error unfriending user:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleCancelSentRequest = async () => {
+    setLoading(true);
+    try {
+      await deleteSentFriendRequest(targetUser.username);
+      setFriendRequestSent(false);
+      setFriendRequestReceived(false);
+      setAreFriends(false);
+
+    } catch (error) {
+      console.error("Error cancelling sent friend request:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleAcceptRequest = async () => {
+    setLoading(true);
+    try {
+      await acceptFriendRequest(targetUser.username);
+      setAreFriends(true);
+      setFriendRequestReceived(false);
+      setFriendRequestSent(false);
+    } catch (error) {
+      console.error("Error accepting friend request:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDeleteReceivedRequest = async () => {
+    setLoading(true);
+    try {
+      await deleteReceivedFriendRequest(targetUser.username);
+      setAreFriends(false);
+      setFriendRequestReceived(false);
+      setFriendRequestSent(false);
+    } catch (error) {
+      console.error("Error deleting received friend request:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSendRequest = async () => {
+    setLoading(true);
+    try {
+      await sendFriendRequest(targetUser.username);
+      setAreFriends(false);
+      setFriendRequestReceived(false);
+      setFriendRequestSent(true);
+    } catch (error) {
+      console.error("Error sending friend request:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="grid place-items-center">
+      <>
+        {areFriends ? (
+          <button disabled={loading}
+            onClick={handleUnfriend}
+            className="px-8 py-3 bg-red-600 rounded-lg">
+            Unfriend {targetUser.username}
+          </button>
+
+        ) : friendRequestSent ? (
+          <button disabled={loading}
+            onClick={handleCancelSentRequest}
+            className="px-8 py-3 bg-red-600 rounded-lg">
+            Cancel sent friend request for {targetUser.username}
+          </button>
+
+        ) : friendRequestReceived ? (
+          <div className="flex gap-4">
+            <button disabled={loading}
+              onClick={handleAcceptRequest}
+              className="px-8 py-3 bg-blue-600 rounded-lg">
+              Accept request from {targetUser.username}
+            </button>
+            <button disabled={loading}
+              onClick={handleDeleteReceivedRequest}
+              className="px-8 py-3 bg-red-600 rounded-lg">
+              Delete request from {targetUser.username}
+            </button>
+          </div>
+
+        ) : (
+          <button disabled={loading}
+            onClick={handleSendRequest}
+            className="px-8 py-3 bg-blue-600 rounded-lg">
+            Send friend request to {targetUser.username}
+          </button>
+        )}
+      </>
+    </div>
+  );
+};
+
+export default FriendStatusViewProfilePage;
