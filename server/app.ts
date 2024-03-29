3cd87aaaf18b9a739f5ddb8349dced8a445a2ed3
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-29 11:31:35 +0100

    gfh

--- server/app.ts
+++ server/app.ts
@@ -40,7 +40,8 @@ const httpServer = createServer(app); // Attach Express app to the HTTP server
 const io = new Server(httpServer, {
     cors: {
         origin: ["http://localhost:5173",
-            "https://social-media-app-kohl-seven.vercel.app"
+            "https://social-media-app-kohl-seven.vercel.app",
+            "https://social-media-app-backend-seven.vercel.app"
         ]
     }
 });
