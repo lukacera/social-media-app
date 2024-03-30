58a26f4b54017e9e73ba5e9479de3cd52ae0d649
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-03-30 15:29:33 +0100

    Update mainPage

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -66,19 +66,16 @@ function MainPage(): ReactNode {
     return (
         <>
             {commentPosted && <GreenMessage text="Comment posted!" />}
-            <div className="grid grid-rows-[15%,85%]
-            lg:h-screen bg-backgroundDark 
-            text-white font-[Nunito]">
+            <div className="grid bg-backgroundDark text-white font-[Nunito]
+            sm:grid-rows-[15%,85%] sm:h-screen">
                 <HeaderNav />
                 <main className=" grid grid-cols-[10%_70%_20%]">
                     <Sidebar />
-
                     <div className="overflow-auto customWebkit">
                         {renderComponent}
                     </div>
-                    <div>
-                        <h3 className="text-[2rem] flex justify-center
-                        font-bold pt-10">
+                    <div className="border-l-2 border-borderGray">
+                        <h3 className="flex justify-center font-bold pt-10 break-words">
                             Messages
                         </h3>
                     </div>
